f725c0e05b7afe430228c323db55edc5a798efab
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2023-09-24 21:39:36 +1000

    DROData: add tests

--- tests/drotrimmer/test_dro_data.py
+++ tests/drotrimmer/test_dro_data.py
@@ -7,10 +7,21 @@ from src.drotrimmer.dro_data import (
     DRODataV2,
     DetailedRegisterInfo,
     DeleteInstructionsCommand,
+    DROInstruction,
+    DROInstructionType,
 )
 from src.drotrimmer.dro_undo import UndoController
 
 SONG_LENGTH = (0xB1 + 0xC100) * 2
+
+
+def shallow_copy_dro_data_v2(dro_data: DRODataV2, new_values: list[int]) -> DRODataV2:
+    return DRODataV2(
+        array.array("B", new_values),
+        dro_data.codemap,
+        dro_data._short_delay_code,
+        dro_data._long_delay_code,
+    )
 
 
 def create_detailed_register_descriptions() -> DetailedRegisterInfo:
@@ -128,6 +139,145 @@ class TestDeleteInstructionsCommand(TestCase):
         self.assertEqual(dro_song.data.data[data_slice], expected_3)
         undo_controller.undo()
         self.assertEqual(dro_song.data.data[data_slice], expected_2)
+
+
+class TestDRODataV2(TestCase):
+    def _compare_dro_data_v2(
+        self,
+        first: DRODataV2,
+        second: DRODataV2,
+        expected_second_data: list[int] | None = None,
+    ) -> None:
+        self.assertEqual(first.codemap, second.codemap)
+        self.assertEqual(first._short_delay_code, second._short_delay_code)
+        self.assertEqual(first._long_delay_code, second._long_delay_code)
+        self.assertEqual(
+            second.data,
+            array.array("B", expected_second_data)
+            if expected_second_data is not None
+            else first.data,
+        )
+
+    def test_del(self) -> None:
+        dro_data = create_dro_data_v2()
+        test_slice = slice(0, 3)
+        self.assertEqual(len(dro_data), 14)
+        self.assertEqual(dro_data.raw_len(), 28)
+        self._compare_dro_data_v2(
+            dro_data[test_slice],
+            shallow_copy_dro_data_v2(dro_data, [0x00, 0x01, 0x02, 0x03, 0x04, 0x05]),
+        )
+
+        # Delete one instruction
+        del dro_data[0]
+        self.assertEqual(len(dro_data), 13)
+        self.assertEqual(dro_data.raw_len(), 26)
+        self._compare_dro_data_v2(
+            dro_data[test_slice],
+            shallow_copy_dro_data_v2(dro_data, [0x02, 0x03, 0x04, 0x05, 0x06, 0x07]),
+        )
+
+        # Delete multiple instructions using a slice
+        del dro_data[1:2]
+        self.assertEqual(len(dro_data), 11)
+        self.assertEqual(dro_data.raw_len(), 22)
+        self._compare_dro_data_v2(
+            dro_data[test_slice],
+            shallow_copy_dro_data_v2(dro_data, [0x02, 0x03, 0x08, 0x09, 0xFE, 0xB0]),
+        )
+
+    def test_get_item(self) -> None:
+        dro_data = create_dro_data_v2()
+        self.assertEqual(
+            dro_data[0],
+            DROInstruction(DROInstructionType.REGISTER, dro_data.codemap[0], 0x01, 0),
+        )
+        self.assertEqual(
+            dro_data[1],
+            DROInstruction(DROInstructionType.REGISTER, dro_data.codemap[2], 0x03, 0),
+        )
+        self.assertEqual(
+            dro_data[2],
+            DROInstruction(DROInstructionType.REGISTER, dro_data.codemap[4], 0x05, 0),
+        )
+        self.assertEqual(
+            dro_data[5],
+            DROInstruction(
+                DROInstructionType.DELAY, dro_data._short_delay_code, 0xB0 + 1, None
+            ),
+        )
+        self._compare_dro_data_v2(
+            dro_data[:2],
+            shallow_copy_dro_data_v2(dro_data, [0x00, 0x01, 0x02, 0x03]),
+        )
+
+    def test_interpret_data(self) -> None:
+        dro_data = create_dro_data_v2()
+
+        self.assertEqual(
+            dro_data._interpret_data(0),
+            DROInstruction(DROInstructionType.REGISTER, dro_data.codemap[0], 0x01, 0),
+        )
+        self.assertEqual(
+            dro_data._interpret_data(2),
+            DROInstruction(DROInstructionType.REGISTER, dro_data.codemap[2], 0x03, 0),
+        )
+        self.assertEqual(
+            dro_data._interpret_data(4),
+            DROInstruction(DROInstructionType.REGISTER, dro_data.codemap[4], 0x05, 0),
+        )
+        self.assertEqual(
+            dro_data._interpret_data(10),
+            DROInstruction(
+                DROInstructionType.DELAY, dro_data._short_delay_code, 0xB0 + 1, None
+            ),
+        )
+
+    def test_is_long_delay(self) -> None:
+        dro_data = create_dro_data_v2()
+        self.assertTrue(dro_data.is_long_delay(0xFF))
+        self.assertFalse(dro_data.is_long_delay(0xFE))
+
+    def test_is_short_delay(self) -> None:
+        dro_data = create_dro_data_v2()
+        self.assertTrue(dro_data.is_short_delay(0xFE))
+        self.assertFalse(dro_data.is_short_delay(0xFF))
+
+    def test_iter(self) -> None:
+        dro_data = create_dro_data_v2()
+        instructions = []
+        for instr in dro_data:
+            instructions.append(instr)
+        self.assertEqual(len(instructions), 14)
+        self.assertEqual(
+            instructions[0],
+            DROInstruction(DROInstructionType.REGISTER, dro_data.codemap[0], 0x01, 0),
+        )
+
+    def test_iter_indexes(self) -> None:
+        dro_data = create_dro_data_v2()
+        iterator = dro_data._iter_indexes()
+        self.assertIsInstance(iterator, range)
+        indexes = list(iterator)
+        self.assertEqual(indexes[:3], [0, 1, 2])
+        self.assertEqual(len(indexes), 14)
+
+    def test_len(self) -> None:
+        dro_data = create_dro_data_v2()
+        self.assertEqual(len(dro_data), 14)
+
+    def test_shallow_copy(self) -> None:
+        dro_data = create_dro_data_v2()
+        dro_data_copy = dro_data.shallow_copy()
+        self._compare_dro_data_v2(dro_data, dro_data_copy, [])
+        dro_data_copy = dro_data.shallow_copy(array.array("B", [1, 2, 3]))
+        self._compare_dro_data_v2(dro_data, dro_data_copy, [1, 2, 3])
+
+    def test_translate_index(self) -> None:
+        dro_data = create_dro_data_v2()
+        self.assertEqual(dro_data._translate_index(0), 0)
+        self.assertEqual(dro_data._translate_index(1), 2)
+        self.assertEqual(dro_data._translate_index(5), 10)
 
 
 class TestDROSongV2(TestCase):
